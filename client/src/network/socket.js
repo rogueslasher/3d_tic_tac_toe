f23cd447c76099605b8de3a09ca6b8a564256720
rogueslasher <aniketpandey25092005@gmail.com>
2026-02-03 20:26:15 +0530

    render port use in socket

--- client/src/network/socket.js
+++ client/src/network/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://threed-tic-tac-toe-rnxj.onrender.com");
 socket.on("connect", () => {
   console.log("Connected to server:", socket.id);
 });
