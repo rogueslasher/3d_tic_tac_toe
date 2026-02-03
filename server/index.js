3bd17e178e6ef3d5db8b81938fa00a621f969226
rogueslasher <aniketpandey25092005@gmail.com>
2026-02-03 20:09:17 +0530

    update server port for render

--- server/index.js
+++ server/index.js
@@ -127,6 +127,8 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
